fb44211f73096cb9c8f4f31290a365ecf96537df
Patil-Kiran <82840059+Patil-Kiran@users.noreply.github.com>
2022-01-08 01:37:05 +0530

    Update SubsequnceOfString.java

--- Recursion/SubsequnceOfString.java
+++ Recursion/SubsequnceOfString.java
@@ -10,22 +10,19 @@ public class SubsequnceOfString {
 		}
 		// Finding first Character
 		char first = str.charAt(0);
-		System.out.println("first ="+first);
+		
 		//Starting from next letter making recursive call !!!! 
 		String remainingString = getSubsequences(str.substring(1));
-		System.out.println("Remaining String ="+remainingString +"   ------count="+(++count));
+		
 		// During Recursive Call we Function calls First variable 
 		// resp we'll get (abc)--->a(bc)--->ab(c)-->abc("")--->empty
-		System.out.println(Arrays.toString(remainingString.split(",", -1)));
+		
 		String result = "";
 		for(String sub:remainingString.split(",", -1)) {
 			result +=","+first +sub;
-			System.out.println("First Result ="+result);
 			result +=","+sub;
-			System.out.println("Second Result ="+result);
 		}
 		return result.substring(1);
-		
 	}
 	public static void main(String[] args) {
 		String str = "abc";
